1f7578b893f7a38fadd867c84e1aa8945f9066f6
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-06-19 17:23:03 -0600

    will get to this later

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int placeNum;
-    float nice = 69.420;
-    printf("\nhello there %.5f\n\n", nice);
+    // int placeNum;
+    // float nice = 69.420;
+    // printf("\nhello there %.5f\n\n", nice);
 
-    scanf("%i", &placeNum);
-    printf("\n %i\n", placeNum);
+    // scanf("%i", &placeNum);
+    // printf("\n %i\n", placeNum);
+
+    float num1;
+    float num2;
+
+    printf("add number: \n");
+    scanf("%f" &num1);
+    printf("+");
+    scanf("%f", &num2);
+    float result = num1 + num2;
+    printf("%f\n", result);
 
     return 0;
 }
